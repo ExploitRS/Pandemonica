8085a825de6ea12a8ce1514eea4c4b9f409b4bbc
John Paul <nil@exploit.rs>
2024-01-01 02:48:14 +0900

    Refactor TaskController store and update methods

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -15,11 +15,21 @@ class TaskController extends Controller
         return response()->json($tasks);
     }
 
-    public function store(StoreTaskRequest $req) {
-        $task = Task::create([
-            'name' => req('name'),
-            'description' => req('description'),
-            'due_date' => req('due_date'),
+    public function store(StoreTaskRequest $request) {
+        $task = new Task([
+            'title' => $request->title,
+            'description' => $request->description,
+            'due_date' => $request->due_date,
         ]);
+
+        $task->save();
+
+    }
+
+    public function update(StoreTaskRequest $request, Task $task) {
+        $task->update($request->all());
+        $task->save();
+
+        // return response()->json($task);
     }
 }
