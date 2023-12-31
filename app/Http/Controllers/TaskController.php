2c960ba681d42cc96d40db5b139fb91094de172a
John Paul <nil@exploit.rs>
2024-01-01 00:31:36 +0900

    Add store method to TaskController

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\Task;
+use App\Http\Requests\StoreTaskRequest;
 
 use Illuminate\Http\Request;
 
@@ -11,5 +12,14 @@ class TaskController extends Controller
     //
     public function index() {
         $tasks = Task::all();
+        return response()->json($tasks);
+    }
+
+    public function store(StoreTaskRequest $req) {
+        $task = Task::create([
+            'name' => req('name'),
+            'description' => req('description'),
+            'due_date' => req('due_date'),
+        ]);
     }
 }
