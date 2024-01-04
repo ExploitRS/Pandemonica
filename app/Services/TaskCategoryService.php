a1742ebbf33e5d85398c211456c3033f1b69a262
John Paul <nil@exploit.rs>
2024-01-05 06:59:03 +0900

    Add add_category method to TaskCategoryService

---
+++ app/Services/TaskCategoryService.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Services;
+
+use Illuminate\Http\Request;
+use App\Models\Task;
+use App\Models\Category;
+use Illuminate\Http\JsonResponse;
+
+class TaskCategoryService
+{
+    public function add_category(Task $task, Category $cat): JsonResponse
+    {
+        // Restrict each task to one category as per the specified requirements
+        if  ($task->categories()->count() > 0) {
+            return response()->json([
+                "message" => "This task already has a category"
+            ], 400);
+        }
+
+        // $task->categories()->attach($cat_id);
+        $task->categories()->attach($cat);
+
+        return response()->json([
+            'message' => 'Category added successfully',
+        ], 200);
+    }
+}
