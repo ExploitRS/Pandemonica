bccd16fa0abc78e54528ff4218707d7276908acc
John Paul <nil@exploit.rs>
2024-01-04 21:59:12 +0900

    Add CommonRequest class for form validation

---
+++ app/Http/Requests/CommonRequest.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Requests;
+
+use App\Http\Controllers\Traits\Response;
+use Illuminate\Contracts\Validation\Validator;
+use Illuminate\Foundation\Http\FormRequest;
+
+use Illuminate\Http\Exceptions\HttpResponseException;
+use Illuminate\Http\JsonResponse;
+
+class CommonRequest extends FormRequest
+{
+    // use Response;
+
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    protected function failedValidation(Validator $validator)
+    {
+        throw new HttpResponseException(
+            response()->json([
+                'message' => 'The given data was invalid.',
+                'data' => $validator->errors(),
+            ], JsonResponse::HTTP_UNPROCESSABLE_ENTITY)
+            // $this->errorResponse($validator->errors(),JsonResponse::HTTP_UNPROCESSABLE_ENTITY)
+        );
+    }
+}
