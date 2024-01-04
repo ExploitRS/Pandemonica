785e31bfa103252de0ebd80933a782a513e9e01f
John Paul <nil@exploit.rs>
2024-01-04 22:01:48 +0900

    Add belongsToMany relation for tasks in Category model

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\belongToMany;
 
 class Category extends Model
 {
@@ -12,4 +13,9 @@ class Category extends Model
     protected $fillable = [
         'label',
     ];
+
+    public function tasks(): belongsToMany
+    {
+        return $this->belongsToMany(Task::class);
+    }
 }
