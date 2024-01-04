122082bdb8d45a73fc3770dade79047bc36a86b2
John Paul <nil@exploit.rs>
2024-01-05 00:14:18 +0900

    Update validation error messages

--- app/Http/Requests/TaskRequest.php
+++ app/Http/Requests/TaskRequest.php
@@ -7,9 +7,9 @@ class TaskRequest extends CommonRequest
     public function messages()
     {
         return [
-            'title.required' => 'A title is required',
-            'title.max' => 'A title cannot be longer than 255 characters',
-            'description.max' => 'A description cannot be longer than 65535 characters',
+            'title.required' => 'The title is required',
+            'title.max' => 'The title cannot be longer than 255 characters',
+            'description.max' => 'The description cannot be longer than 65535 characters',
             'due_date.date' => 'The due date must be a valid date',
             'due_date.after_or_equal' => 'The due date must be today or later',
         ];
